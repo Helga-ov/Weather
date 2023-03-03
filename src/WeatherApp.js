5e24a2dc04548f9270e36a439f4889c4a9d514dc
Helga-ov <115106871+Helga-ov@users.noreply.github.com>
2023-03-03 20:59:51 +0100

    all

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,83 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import WeatherCharacteristics from "./WeatherCharacteristics";
 import "./WeatherApp.css";
-// import axios from "axios";
-// import Search from "./Search";
 
-export default function WeatherApp(props) {
-  return (
-    <div className="WeatherApp">
-      <div className="fild">
-        <div className="WeatherFild">
-          <div className="container">
-            <div className="row d-flex justify-content-center">
-              <form id="searchForm">
-                <input
-                  type="text"
-                  className="city2"
-                  id="city1"
-                  required
-                  placeholder="Enter your city"
-                />
-                <button type="submit" className="search" /*id="search1"*/>
-                  Search
-                </button>
-              </form>
-            </div>
-            <div className="row d-flex justify-content-between mt-4">
-              <div className="col-sm-7">
-                <button className="current">Current weather</button>
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
+
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      coordinates: response.data.coord,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      date: new Date(response.data.dt * 1000),
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+      min: Math.round(response.data.main.temp_min),
+      max: Math.round(response.data.main.temp_max),
+      feelsLike: Math.round(response.data.main.feels_like),
+    });
+  }
+
+  function search() {
+    const apiKey = "0ebc654fccbc00189d5408f3d6f15b08";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="WeatherApp">
+        <div className="fild">
+          <div className="WeatherFild">
+            <div className="container">
+              <div className="row d-flex justify-content-center">
+                <form onSubmit={handleSubmit}>
+                  <input
+                    type="text"
+                    className="city2"
+                    required
+                    placeholder="Enter your city"
+                    onChange={handleCityChange}
+                  />
+                  <button type="submit" className="search">
+                    Search
+                  </button>
+                </form>
               </div>
-              <div className="col-sm-5 justify-content-left">
-                <p className="lc" id="lc">
-                  Vienna
-                </p>
-              </div>
-            </div>
-            <div className="row d-flex justify-content-between">
-              <div className="col-sm-7">
-                <img
-                  src="https://em-content.zobj.net/thumbs/120/google/350/sun-behind-cloud_26c5.png"
-                  alt=""
-                  className="icon"
-                  id="icon1"
-                />
-                <p className="temperature" id="temperature">
-                  6°C
-                </p>
-              </div>
-              <div className="col-sm-5">
-                <p className="currentTime" /*id="currentTime"*/>
-                  {/* <Time /> */}
-                  Saturday 15:34
-                </p>
-                <p className="ffh">
-                  Feels like: <span id="feelsLike">-4°C</span>
-                  <br />
-                  Forecast:
-                  <span id="minTemp"> 1°C</span>/<span id="maxTemp">4°C</span>
-                  <br />
-                  Humidity:
-                  <span id="humidity"> 60%</span>
-                  <br />
-                  Wind:
-                  <span id="wind">0 km/h</span>
-                </p>
-              </div>
-            </div>
-            <div className="row d-flex justify-content-start">
-              <div className="description" id="description-id">
-                Few clouds
-              </div>
+              <WeatherCharacteristics data={weatherData} />
             </div>
           </div>
+          <p className="owner">
+            This website was coded by Olha Ovsii, and is{" "}
+            <a href="https://github.com/Helga-ov/Weather" class="link">
+              open-sourced
+            </a>
+          </p>
         </div>
-        <p className="owner">
-          This website was coded by Olha Ovsii, and is{" "}
-          <a href="https://github.com/Helga-ov/Weather" class="link">
-            open-sourced
-          </a>
-        </p>
       </div>
-    </div>
-  );
+    );
+  } else {
+    search();
+    return "Loading...";
+  }
 }
---
+++ src/WeatherCharacteristics.js
@@ -0,0 +1,47 @@
+import React from "react";
+import CurrentTime from "./CurrentTime";
+import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
+import "./WeatherApp.css";
+
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInformation">
+      <div className="row d-flex justify-content-between mt-4">
+        <div className="col-sm-7">
+          <button className="current">Current weather</button>
+        </div>
+        <div className="col-sm-5 justify-content-right">
+          <p className="lc">{props.data.city}</p>
+        </div>
+      </div>
+      <div className="row d-flex justify-content-between">
+        <div className="col-sm-7">
+          <WeatherIcon code={props.data.icon} className="icon" size={100} />
+          <p className="temperature">
+            <WeatherTemperature celsius={Math.round(props.data.temperature)} />
+          </p>
+        </div>
+
+        <div className="col-sm-5">
+          <p className="currentTime">
+            <CurrentTime date={props.data.date} />
+          </p>
+          <p className="ffh">
+            Feels like: {props.data.feelsLike}°C
+            <br />
+            Forecast: {props.data.min}°C/
+            {props.data.max}°C
+            <br />
+            Humidity: {props.data.humidity} %
+            <br />
+            Wind: {Math.round(props.data.wind)} km/h
+          </p>
+        </div>
+      </div>
+      <div className="row d-flex justify-content-start">
+        <div className="description">{props.data.description}</div>
+      </div>
+    </div>
+  );
+}
